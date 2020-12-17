529b70d9ec14afab27ca9994a6f6d2e05066463e
Vishwamagdavan <vishwamagadavan@gmail.com>
2020-12-17 13:17:30 +0530

    updated UI

--- client/src/components/Mainscreen.js
+++ client/src/components/Mainscreen.js
@@ -1,6 +1,7 @@
 import { React, useState, useEffect } from "react";
-import { ThemeProvider, Paper, makeStyles, Grid, Typography, Box, Select,MenuItem,Button } from "@material-ui/core";
+import { ThemeProvider, Paper, makeStyles, Grid, Typography, Box, Select, MenuItem, Button, Tabs, Tab, AppBar } from "@material-ui/core";
 import { Redirect } from "react-router-dom";
+import PropTypes from 'prop-types';
 import ScrollToBottom from 'react-scroll-to-bottom';
 import queryString from 'query-string';
 import { ControlledEditor } from "@monaco-editor/react";
@@ -12,24 +13,65 @@ import Message from './Chatbox/Message'
 let socket;
 
 const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+    },
     editor__papper: {
         marginTop: 8
     },
-
+    tabs: {
+        borderRight: `1px solid ${theme.palette.divider}`,
+    },
 
 })
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
 function Mainscreen({ location }) {
     const classes = useStyles();
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [value, setValue] = useState(0);
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
     const [code, setCode] = useState('//Write your Code');
     const [codeupdate, setCodeupdate] = useState('')
-    const [language,setLanguage]=useState('cpp');
-    const[fontsize,setFontsize]=useState(20);
-    const [selflanguage,setSelflanguage]=useState('cpp');
-    const[selffontsize,setSelffontsize]=useState(20);
+    const [language, setLanguage] = useState('cpp');
+    const [fontsize, setFontsize] = useState(20);
+    const [selflanguage, setSelflanguage] = useState('cpp');
+    const [selffontsize, setSelffontsize] = useState(20);
     const ENDPOINT = 'localhost:5000';
 
     const options = {
@@ -42,6 +84,10 @@ function Mainscreen({ location }) {
     function onchangeHandler(ev, event) {
         setCodeupdate(event);
     }
+    const tabhandleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     // Socket for Joining Room
 
     useEffect(() => {
@@ -92,89 +138,95 @@ function Mainscreen({ location }) {
             <Paper style={{ height: "100vh" }} >
                 <Navbar />
                 <Paper className={classes.editor__papper}>
-                        
-
                     <Grid container justify="flex-start">
-                        <Grid item xs={false} sm={5} md={5}>
-                            <h1>Live Code</h1>
-                        Language:
-                        <Select labelId="language" id="select" value={language} style={{padding:10}} onChange={(e)=>{setLanguage(e.target.value);}}>
-                            <MenuItem value="javascript">Javascript</MenuItem>
-                            <MenuItem value="cpp">C++</MenuItem>
-                            <MenuItem value="c">C</MenuItem>
-                            <MenuItem value="java">Java</MenuItem>
-                            <MenuItem value="python">Python</MenuItem>
-                        </Select>
+                    <Grid item xs={false} sm={9} md={9}>
+                            <AppBar position="static">
+                                <Tabs value={value} onChange={tabhandleChange} aria-label="simple tabs example"  variant="fullWidth">
+                                    <Tab label="Live Code" {...a11yProps(0)} />
+                                    <Tab label="Your Code" {...a11yProps(1)} />
+                                    <Tab label="Canvas" {...a11yProps(2)} />
+                                </Tabs>
+                            </AppBar>
+
+                        <TabPanel value={value} index={0}>
+                            Language:
+                        <Select labelId="language" id="select" value={language} style={{ padding: 10 }} onChange={(e) => { setLanguage(e.target.value); }}>
+                                    <MenuItem value="javascript">Javascript</MenuItem>
+                                    <MenuItem value="cpp">C++</MenuItem>
+                                    <MenuItem value="c">C</MenuItem>
+                                    <MenuItem value="java">Java</MenuItem>
+                                    <MenuItem value="python">Python</MenuItem>
+                                </Select>
                         FontSize:
-                        <Select labelId="fontsize" id="select" value={fontsize} onChange={(e)=>{setFontsize(e.target.value)}}>
-                            <MenuItem value="5">5</MenuItem>
-                            <MenuItem value="10">10</MenuItem>
-                            <MenuItem value="15">15</MenuItem>
-                            <MenuItem value="20">20</MenuItem>
-                            <MenuItem value="25">25</MenuItem>
-                            <MenuItem value="30">30</MenuItem>
-                            <MenuItem value="35">35</MenuItem>
-                        </Select>
-                            <ControlledEditor
-                                width="80vh"
-                                height="60vh"
-                                language={language}
-                                theme="dark"
-                                value={code}
-                                options={options}
-                                onChange={onchangeHandler}
-                            />
-                        </Grid>
-                        <Grid item xs={false} sm={5} md={5}>
-                        <h1>Your Code</h1>
-                        Language:
-                        <Select labelId="language" id="select" value={selflanguage} style={{padding:10}} onChange={(e)=>{setSelflanguage(e.target.value);}}>
-                            <MenuItem value="javascript">Javascript</MenuItem>
-                            <MenuItem value="cpp">C++</MenuItem>
-                            <MenuItem value="c">C</MenuItem>
-                            <MenuItem value="java">Java</MenuItem>
-                            <MenuItem value="python">Python</MenuItem>
-                        </Select>
-                        FontSize:
-                        <Select labelId="fontsize" id="select" value={selffontsize} onChange={(e)=>{setSelffontsize(e.target.value)}}>
-                            <MenuItem value="5">5</MenuItem>
-                            <MenuItem value="10">10</MenuItem>
-                            <MenuItem value="15">15</MenuItem>
-                            <MenuItem value="20">20</MenuItem>
-                            <MenuItem value="25">25</MenuItem>
-                            <MenuItem value="30">30</MenuItem>
-                            <MenuItem value="35">35</MenuItem>
-                        </Select>
-                            <ControlledEditor
-                                width="80vh"
-                                height="60vh"
-                                language={selflanguage}
-                                theme="dark"
-                                options={selfoptions}
-                            />
-                        <Button type="submit" variant="contained" color="secondary">Compile</Button>
-                        </Grid>
-                        <Grid item sm={2} md={2}>
-                            <Box>
-                            <h1>Message Box</h1>
+                        <Select labelId="fontsize" id="select" value={fontsize} onChange={(e) => { setFontsize(e.target.value) }}>
+                                    <MenuItem value="5">5</MenuItem>
+                                    <MenuItem value="10">10</MenuItem>
+                                    <MenuItem value="15">15</MenuItem>
+                                    <MenuItem value="20">20</MenuItem>
+                                    <MenuItem value="25">25</MenuItem>
+                                    <MenuItem value="30">30</MenuItem>
+                                    <MenuItem value="35">35</MenuItem>
+                                </Select>
+                                <ControlledEditor
+                                    width="100vh"
+                                    height="70vh"
+                                    language={language}
+                                    theme="dark"
+                                    value={code}
+                                    options={options}
+                                    onChange={onchangeHandler}
+                                />
+                        </TabPanel>
+                        <TabPanel value={value} index={1}>
+                            Language:
+                        <Select labelId="language" id="select" value={selflanguage} style={{ padding: 10 }} onChange={(e) => { setSelflanguage(e.target.value); }}>
+                                    <MenuItem value="javascript">Javascript</MenuItem>
+                                    <MenuItem value="cpp">C++</MenuItem>
+                                    <MenuItem value="c">C</MenuItem>
+                                    <MenuItem value="java">Java</MenuItem>
+                                    <MenuItem value="python">Python</MenuItem>
+                                </Select>
+                                FontSize:
+                        <Select labelId="fontsize" id="select" value={selffontsize} onChange={(e) => { setSelffontsize(e.target.value) }}>
+                                    <MenuItem value="5">5</MenuItem>
+                                    <MenuItem value="10">10</MenuItem>
+                                    <MenuItem value="15">15</MenuItem>
+                                    <MenuItem value="20">20</MenuItem>
+                                    <MenuItem value="25">25</MenuItem>
+                                    <MenuItem value="30">30</MenuItem>
+                                    <MenuItem value="35">35</MenuItem>
+                                </Select>
+                                <ControlledEditor
+                                    width="100vh"
+                                    height="70vh"
+                                    language={selflanguage}
+                                    theme="dark"
+                                    options={selfoptions}
+                                />
+                                <Button type="submit" variant="contained" color="secondary">Compile</Button>
+                        </TabPanel>
+
+                    </Grid>
+                    <Grid item sm={3} md={3}>
+                            <Box justifyContent="center">
+                                <h1>Message Box</h1>
                                 <Typography variant="body1">Room:{room}</Typography>
                                 <Typography variant="body1">Your Name:{name}</Typography>
-                                
+
                                 <form className={classes.form}>
                                     <input
                                         placeholder="type your message"
                                         value={message}
                                         onChange={(e) => setMessage(e.target.value)}
                                         onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null} />
-                                    <input type="submit"  placeholder="send"/>
+                                    <input type="submit" placeholder="send" />
                                 </form>
                                 <ScrollToBottom>
                                     {messages.map((m, i) => <div key={i}><Message name={name} message={m} /></div>)}
                                 </ScrollToBottom>
                             </Box>
+                            </Grid>
                         </Grid>
-                    </Grid>
-
                 </Paper>
 
             </Paper>
